4e7b664d4a550152f0d7c7e11872e823433b02ab
Abubakr4Dev <133663527+Abubakr4Dev@users.noreply.github.com>
2024-06-11 10:48:49 +0200

    Mongo & Docker configurations done.

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //     transform: true,
+  //     transformOptions: { enableImplicitConversion: true },
+  //   }),
+  // );
+  const configService: ConfigService = app.get(ConfigService);
+  const port: number = configService.get<number>('PORT');
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://example.com',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'UPDATED', 'PUT', 'PATCH'],
+    credentials: true,
+  });
+  await app.listen(port);
+}
+bootstrap();
